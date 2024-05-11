7e65076bba7e66d526dc769239203beaf5432d9c
basmala-ayman <basmala.ayman.1904@gmail.com>
2024-05-11 10:15:05 +0300

    fininshing ranking

--- DisplayRank.cpp
+++ DisplayRank.cpp
@@ -5,9 +5,7 @@ DisplayRank::DisplayRank(QWidget* parent): QMainWindow(parent), ui(new Ui::Displ
     ui->setupUi(this);
     connect(ui->back, SIGNAL(triggered()), this, SLOT(moveToOperations()));
     connect(ui->next, SIGNAL(clicked()), this, SLOT(moveToFinal()));
-    //populateWordRanksTable();
     
-    //multimap<int, std::string> rankedVector = populateWordRanksTable();
     vector<pair<string, int>> rankedVector = populateWordRanksTable();
 
     ui->tableWidget->setRowCount(rankedVector.size());
@@ -42,11 +40,6 @@ void DisplayRank::moveToFinal()
     finalPage->show();
 }
 
-DisplayRank::~DisplayRank()
-{
-    delete ui;
-}
-
 vector<pair<string, int>> DisplayRank::populateWordRanksTable()
 {
     multimap<int, string, greater<int>> sortedMap = SortDesc::sortWordsDesc();
@@ -64,64 +57,13 @@ vector<pair<string, int>> DisplayRank::populateWordRanksTable()
                 r.push_back(make_pair(pair.second, rank++));
                 continue;
             }
-            /*else
-            {
-                r.push_back(make_pair(pair.second, rank));
-            }*/
         }
         r.push_back(make_pair(pair.second, rank));
-
-        /*else
-        {
-            r.push_back(make_pair(pair.second, rank));
-        }*/
     }
     return r;
 }
 
-//std::map<std::string, int> DisplayRank::populateWordRanksTable()
-//{
-//    std::multimap<int, std::string, std::greater<int>> sortedMap = SortDesc::sortWordsDesc();
-//
-//    //unordered_map<int, int> rankMap;  // Map to store rank for each frequency
-//    int rank = 1;
-//
-//    std::map<std::string, int> r;
-//    for (auto pair : sortedMap) {
-//        //int frequency = pair.first;
-//
-//        r[pair.second] = rank;
-//        rank++;
-//    }
-//    return r;
-//}
-
-//std::multimap<int,std::string> DisplayRank::populateWordRanksTable()
-//{
-//    std::multimap<int, std::string> rankedWords;
-// 
-//    std::multimap<int, std::string, std::greater<int>> sortedMap = SortDesc::sortWordsDesc();
-//    int rank = 0;
-//    for (auto word: sortedMap)
-//    {
-//        int frequency = word.first;
-//
-//        if (rankedWords.end()->first!=frequency) {
-//            rank++;
-//        }
-//        rankedWords.insert({ word.first, word.second });
-//    }
-//    return rankedWords;
-//}
-
-//void DisplayRank::displayWordRanks()
-//{
-//    // Displaying word ranks via QMessageBox
-//    multimap<int, string, greater<int>> sortedMap = SortDesc::sortWordsDesc();
-//    QString output = "Word ranks based on frequency:\n";
-//    int rank = 1;
-//    for (const auto& pair : sortedMap) {
-//        output += QString::fromStdString(pair.second) + "\t" + QString::number(rank++) + "\n";
-//    }
-//    QMessageBox::information(this, "Word Ranks", output);
-//}
+DisplayRank::~DisplayRank()
+{
+    delete ui;
+}
