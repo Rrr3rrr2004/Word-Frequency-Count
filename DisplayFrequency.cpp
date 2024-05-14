abb09d2f9a54c3bbdf0fb28ac813022a1aaf00fd
basmala-ayman <basmala.ayman.1904@gmail.com>
2024-05-14 17:34:20 +0300

    displayFreqTable

--- DisplayFrequency.cpp
+++ DisplayFrequency.cpp
@@ -8,7 +8,26 @@ DisplayFrequency::DisplayFrequency(QWidget *parent)
     ui->tableFreq->setRowCount(GlobalFunctions::localFrequencies.size());
     connect(ui->next, SIGNAL(clicked()), this, SLOT(moveToFinal()));
 	connect(ui->back, SIGNAL(triggered()), this, SLOT(moveToOperations()));
+    displayFreqTable();
+    //// Populate table
+    //int row = 0;
+    //for (auto pair : GlobalFunctions::localFrequencies)
+    //{
+    //    ui->tableFreq->setItem(row, 0, new QTableWidgetItem(QString::fromStdString(pair.first)));
+    //    ui->tableFreq->setItem(row, 1, new QTableWidgetItem(QString::number(pair.second)));
+    //    for (auto pair2 : GlobalFunctions::globalFrequencies)
+    //    {
+    //        if (pair.first == pair2.first)
+    //        {
+    //            ui->tableFreq->setItem(row, 2, new QTableWidgetItem(QString::number(pair2.second)));
+    //        }
+    //    }
+    //    ++row;
+    //}
+}
 
+void DisplayFrequency::displayFreqTable()
+{
     // Populate table
     int row = 0;
     for (auto pair : GlobalFunctions::localFrequencies)
